c6dd2938575b5e4482d539453373898860642a83
reiv <javierromaynieto@gmail.com>
2024-01-22 12:56:43 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArray = str.split("");
+    const newArray = [];
+    for (char of strArray) {
+        newArray.unshift(char);
+    }
+    return newArray.join("");
 };
 
 // Do not edit below this line
