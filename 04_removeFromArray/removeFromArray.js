5fccc921df01e9bde47323c8fd5dba8e9dcc74b4
reiv <javierromaynieto@gmail.com>
2024-01-23 09:49:05 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    const elementsToRemove = [...elements]
 
+    for (el of elementsToRemove) {
+        if (arr.indexOf(el) !== -1) {
+            const index = arr.indexOf(el);
+            arr.splice(index, 1);
+        }
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
