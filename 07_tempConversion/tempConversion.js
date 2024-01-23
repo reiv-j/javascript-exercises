9e7a0983e67e530a52c331653b7c182c69f5870f
reiv <javierromaynieto@gmail.com>
2024-01-23 12:33:38 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const result = 5/9 * (temp - 32);
+  return Number(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const result = (temp * (9/5)) + 32;
+  return Number(result.toFixed(1));
 };
 
 // Do not edit below this line
