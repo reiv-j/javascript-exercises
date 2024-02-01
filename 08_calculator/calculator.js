0b08d03403e94e4cc0b83459dd2d5fa01852de7b
reiv <javierromaynieto@gmail.com>
2024-02-01 10:10:04 +0100

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((a,b) => a + b);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((a,b) => a * b);
+  }
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+  let result = 1;
+	for (let i = 0; i < power; i++) {
+    result *= num1;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let result = num;
+  for (let i = num - 1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
