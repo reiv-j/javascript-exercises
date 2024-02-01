3739137edfdfdfedfbb02dcea3052429662bb94c
reiv <javierromaynieto@gmail.com>
2024-02-01 12:17:36 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
-};
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
+
+    const arr = [1, 1]
+    for (let i = num - 2; i > 0; i--) {
+      arr.push(arr.at(-1) + arr.at(-2))
+    }
+    return arr.at(-1);
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
