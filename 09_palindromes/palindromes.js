b76692f86c25fbbc2b71c1abf21cdeb3aefc4949
reiv <javierromaynieto@gmail.com>
2024-02-01 12:06:51 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    const alphaNum = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+    
+    const splitArr = str.split("").map(char => char.toLowerCase());
+    const onlyLetters = splitArr.filter(char => alphaNum.includes(char));
+    const finalArr = [];
+    onlyLetters.map(char => finalArr.unshift(char));
+  
+    let result = true;
+    for (let i = 0; i < finalArr.length; i++) {
+      if (finalArr[i] === onlyLetters[i]) {
+        continue;
+      } else {
+        result = false;
+      }
+    }
+  
+    return result;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
