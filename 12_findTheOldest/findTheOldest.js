74edf720e9b4a85fcd572f06b4aa0353acacae5b
reiv <javierromaynieto@gmail.com>
2024-02-05 12:48:18 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((prevPerson, nextPerson) => {
+        if (prevPerson.yearOfDeath - prevPerson.yearOfBirth > nextPerson.yearOfDeath - nextPerson.yearOfBirth) {
+            return prevPerson;
+        } else {
+            return nextPerson;
+        }
+    })
 
+    return oldest;
 };
 
 // Do not edit below this line
